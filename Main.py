316d9339c0eacbd8d1e90256f4a39ae092a2819a
ToppiOfficial <toppi.official@gmail.com>
2023-11-03 18:18:34 +0800

    Added Comments

--- Main.py
+++ Main.py
@@ -9,6 +9,8 @@ class Periodogram:
         self.file_path = file_path
         self.data = pd.read_excel(self.file_path)
 
+    # Process a specified row of data from the excel sheet. It also filters out
+    # any data that is exactly 0.
     def process_periodogram_data(self, column_name):
         time_series_data = [float(row[column_name]) for index, row in self.data.iterrows()]
         frequencies, power_spectrum = periodogram(time_series_data)
@@ -18,16 +20,20 @@ class Periodogram:
 
         return nonzero_freq, nonzero_power
 
+    # Computes the data into a periodogram (refer to the last method).
+    # This is done to separate on computing the original data and computing and smoothing of data
     def compute_periodogram(self, column_name):
         nonzero_freq, nonzero_power = self.process_periodogram_data(column_name)
         return nonzero_freq, nonzero_power
 
+    # Like the previous method, this simply process for periodogram but also smoothen the results by
+    # applying low pass filtering.
     def compute_smooth_periodogram(self, column_name, filter_size=10):
         nonzero_freq, nonzero_power = self.process_periodogram_data(column_name)
         smoothed_power_spectrum = np.convolve(nonzero_power, np.ones(filter_size) / filter_size, mode='same')
         return nonzero_freq, smoothed_power_spectrum
 
-    # Removes any data that is 0
+    # Plots the data on a graph and export the graph in a jpg image
     def plot_periodogram(self, frequencies, power_spectrum, column_name):
 
         plt.figure(figsize=(8, 4))
@@ -45,13 +51,16 @@ class Periodogram:
         plt.savefig(file_path, format='jpg')
         plt.show()
 
+# Specify the excel sheet
 DataSheet = Periodogram('DSP Data Collection.xlsx')
 
+# Compute and plot a periodogram for temperature
 frequencies, power_spectrum = DataSheet.compute_periodogram("Temperature (Celsius)")
 sm_frequencies, sm_power_spectrum = DataSheet.compute_smooth_periodogram("Temperature (Celsius)")
 DataSheet.plot_periodogram(frequencies, power_spectrum, "Temperature")
 DataSheet.plot_periodogram(sm_frequencies, sm_power_spectrum, "Temperature_Smoothen")
 
+# Compute and plot a periodogram for humidity
 frequencies, power_spectrum = DataSheet.compute_periodogram("Humidity")
 sm_frequencies, sm_power_spectrum = DataSheet.compute_smooth_periodogram("Humidity")
 DataSheet.plot_periodogram(frequencies, power_spectrum, "Humidity")
