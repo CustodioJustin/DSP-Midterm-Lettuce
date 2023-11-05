9fb50d10b2428bba140a7c81c0d128e2e3526544
ToppiOfficial <toppi.official@gmail.com>
2023-11-05 23:41:41 +0800

    Updated to include daily and revision of code and comments.

--- Main.py
+++ Main.py
@@ -6,13 +6,19 @@ from scipy.signal import periodogram
 
 class Periodogram:
     def __init__(self, file_path):
+        """
+        Initializes the Periodogram class, setting the file path for data retrieval.
+        """
         self.file_path = file_path
         self.data = pd.read_excel(self.file_path)
 
-    # Process a specified row of data from the excel sheet. It also filters out
-    # any data that is exactly 0.
     def process_periodogram_data(self, column_name):
-        time_series_data = [float(row[column_name]) for index, row in self.data.iterrows()]
+        """
+        Processes the data for a specified column by computing the periodogram.
+        Edit: Adds a method to skip rows where data is empty.  This must be added else
+        graphing daily and weekly isn't correct
+        """
+        time_series_data = [float(row[column_name]) for index, row in self.data.iterrows() if not pd.isnull(row[column_name])]
         frequencies, power_spectrum = periodogram(time_series_data)
 
         nonzero_freq = [freq for freq, power in zip(frequencies, power_spectrum) if freq != 0]
@@ -20,25 +26,31 @@ class Periodogram:
 
         return nonzero_freq, nonzero_power
 
-    # Computes the data into a periodogram (refer to the last method).
-    # This is done to separate on computing the original data and computing and smoothing of data
     def compute_periodogram(self, column_name):
+        """
+        Computes the periodogram for the specified column, utilizing processed data.
+        """
         nonzero_freq, nonzero_power = self.process_periodogram_data(column_name)
         return nonzero_freq, nonzero_power
 
-    # Like the previous method, this simply process for periodogram but also smoothen the results by
-    # applying low pass filtering.
     def compute_smooth_periodogram(self, column_name, filter_size=10):
+        """
+        Computes a smoothed periodogram by applying low pass filtering to the data.
+        """
         nonzero_freq, nonzero_power = self.process_periodogram_data(column_name)
         smoothed_power_spectrum = np.convolve(nonzero_power, np.ones(filter_size) / filter_size, mode='same')
         return nonzero_freq, smoothed_power_spectrum
 
-    # Plots the data on a graph and export the graph in a jpg image
     def plot_periodogram(self, frequencies, power_spectrum, column_name):
-
+        """
+        Plots the periodogram graph based on computed frequencies and power spectrum.
+        Saves the graph as an image file and displays it.
+        """
         plt.figure(figsize=(8, 4))
         plt.semilogy(frequencies, power_spectrum)
-        plt.title('Periodogram')
+        plt.title(f'{column_name} - Periodogram')
+        file_name = f'{column_name}_Periodogram.png'
+
         plt.xlabel('Frequency')
         plt.ylabel('Power Spectral Density')
         plt.grid(True)
@@ -47,21 +59,43 @@ class Periodogram:
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        file_path = os.path.join(directory, f'{column_name}_periodogram.png')
-        plt.savefig(file_path, format='jpg')
+        file_path = os.path.join(directory, file_name)
+        plt.savefig(file_path, format='png', dpi=300)
         plt.show()
 
-# Specify the excel sheet
 DataSheet = Periodogram('DSP Data Collection.xlsx')
 
-# Compute and plot a periodogram for temperature
+# Displaying non-smooth periodogram for Temperature
 frequencies, power_spectrum = DataSheet.compute_periodogram("Temperature (Celsius)")
-sm_frequencies, sm_power_spectrum = DataSheet.compute_smooth_periodogram("Temperature (Celsius)")
-DataSheet.plot_periodogram(frequencies, power_spectrum, "Temperature")
-DataSheet.plot_periodogram(sm_frequencies, sm_power_spectrum, "Temperature_Smoothen")
+DataSheet.plot_periodogram(frequencies, power_spectrum, "Temperature - 15m Interval")
 
-# Compute and plot a periodogram for humidity
+# Displaying non-smooth periodogram for Humidity
 frequencies, power_spectrum = DataSheet.compute_periodogram("Humidity")
-sm_frequencies, sm_power_spectrum = DataSheet.compute_smooth_periodogram("Humidity")
-DataSheet.plot_periodogram(frequencies, power_spectrum, "Humidity")
-DataSheet.plot_periodogram(sm_frequencies, sm_power_spectrum, "Humidity_Smoothen")
+DataSheet.plot_periodogram(frequencies, power_spectrum, "Humidity - 15m Interval")
+
+# Displaying non-smooth periodogram for Temperature Daily
+frequencies, power_spectrum = DataSheet.compute_periodogram("Temperature - Daily")
+DataSheet.plot_periodogram(frequencies, power_spectrum, "Temperature - Daily")
+
+# Displaying non-smooth periodogram for Humidity Daily
+frequencies, power_spectrum = DataSheet.compute_periodogram("Humidity - Daily")
+DataSheet.plot_periodogram(frequencies, power_spectrum, "Humidity - Daily")
+
+
+
+# Displaying smooth periodogram for Temperature
+frequencies, power_spectrum = DataSheet.compute_smooth_periodogram("Temperature (Celsius)")
+DataSheet.plot_periodogram(frequencies, power_spectrum, "Temperature - 15m Interval (Low Pass Filter)")
+
+# Displaying smooth periodogram for Humidity
+frequencies, power_spectrum = DataSheet.compute_smooth_periodogram("Humidity")
+DataSheet.plot_periodogram(frequencies, power_spectrum, "Humidity - 15m Interval (Low Pass Filter)")
+
+# Displaying smooth periodogram for Temperature Daily
+frequencies, power_spectrum = DataSheet.compute_smooth_periodogram("Temperature - Daily")
+DataSheet.plot_periodogram(frequencies, power_spectrum, "Temperature - Daily (Low Pass Filter)")
+
+# Displaying smooth periodogram for Humidity Daily
+frequencies, power_spectrum = DataSheet.compute_smooth_periodogram("Humidity - Daily")
+DataSheet.plot_periodogram(frequencies, power_spectrum, "Humidity - Daily (Low Pass Filter)")
+
